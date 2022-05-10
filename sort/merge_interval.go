4ceba9307f77836b753ad3e17053dade33f62b8c
ccwang <ccxhwmy@gmail.com>
2022-05-10 23:22:15 +0800

    merge intervals

---
+++ sort/merge_interval.go
@@ -0,0 +1,27 @@
+package sort
+
+import "sort"
+
+func merge(intervals [][]int) [][]int {
+	if len(intervals) == 0 {
+		return intervals
+	}
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] == intervals[j][0] {
+			return intervals[i][1] <= intervals[j][1]
+		} else {
+			return intervals[i][0] <= intervals[j][0]
+		}
+	})
+	for i := 0; i < len(intervals) - 1; {
+		if intervals[i][1] >= intervals[i+1][0] {
+			if intervals[i][1] < intervals[i+1][1] {
+				intervals[i][1] = intervals[i+1][1]
+			}
+			intervals = append(intervals[:i+1], intervals[i+2:]...)
+		} else {
+			i++
+		}
+	}
+	return intervals
+}
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -6,3 +6,8 @@ func TestQuickSort(t *testing.T)  {
 	nums := []int{5,2,3,1}
 	t.Log(quickSort(nums))
 }
+
+func TestMerge(t *testing.T) {
+	//t.Log(merge([][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}))
+	t.Log(merge([][]int{{1, 4}, {0, 2}, {3, 5}}))
+}
